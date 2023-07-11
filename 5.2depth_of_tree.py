6170e7162852110e3c6fe28810a30ffb3a638b9d
sangitakarki <77866879+sangitakarki@users.noreply.github.com>
2023-07-11 16:15:30 -0400

    Update 5.2depth_of_tree.py

--- 5.2depth_of_tree.py
+++ 5.2depth_of_tree.py
@@ -8,8 +8,7 @@ from Bio.Align import MultipleSeqAlignment
 from Bio import Phylo
 
 list1 = []
-#tree_file = "/projects/Aylward_Lab/sangita/third_project/Pfam_all_files/pfam_domain_trees/all_new_files/cytoskeleton_trees/cytoskeleton/GED_trim.aln.treefile"
-tree_file = "/projects/Aylward_Lab/sangita/third_project/Pfam_all_files_2/phylo_clusters/GED_umtree.nwk"
+tree_file = "protein_umtree.nwk"
 #trees = Phylo.parse (tree_file, "newick")
 for tree in Phylo.parse(tree_file, "newick"):
     #tree = tree.root_at_midpoint()
