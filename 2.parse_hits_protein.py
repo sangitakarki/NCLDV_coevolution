7d49d1cd16e07f5f6f341525168d19ea94ae9dad
sangitakarki <77866879+sangitakarki@users.noreply.github.com>
2023-07-11 16:04:46 -0400

    Update 2.parse_hits_protein.py

--- 2.parse_hits_protein.py
+++ 2.parse_hits_protein.py
@@ -10,12 +10,8 @@ from Bio.Seq import Seq
 
 ####1. Working with all_euk_pfamout.txt file, splitting by ; and putting in different lines
 
-#df_domain = pd.read_csv("final_reduced_euk_proteins.parsed", header=None) ##pfamout.txt file
-#df_domain = pd.read_csv("all_complete_NCLDV_proteins.parsed", header=None)
-#df_domain = pd.read_csv("merged_bac_familyreps.parsed", header=None)
-df_domain = pd.read_csv("all_asgard_proteins_updated.parsed", header=None)
-#df_domain = pd.read_csv("cured_merged_arconly_familyreps.parsed", header=None)
-#df_domain = pd.read_csv("all_complete_Mirus_proteins.parsed", header=None)
+
+df_domain = pd.read_csv("parse_file.txt", header=None)
 
 
 
@@ -40,8 +36,6 @@ for row in df_domain.iterrows():
 df_domain_new = pd.DataFrame({"id": idxs, "values": values, "scores": scores})
 #print (df_domain_new)
 
-###2. Working with euk_sign_protein files that are only shared (asgard, GV and euk)
-### not interested in ESPs present only in euk
 
 ###IMP#### df_esp = pd.read_csv("ESPs_example.txt", sep = "\t")
 df_esp = pd.read_csv("ESPs.tsv", sep = "\t")
@@ -54,18 +48,8 @@ hit_protein_dict = new_df.groupby("values")["id"].apply(list).to_dict()
 ########
 dict_id_seq = {}
 
-for record in SeqIO.parse("all_asgard_proteins_updated.faa", "fasta"):
-#for record in SeqIO.parse("all_complete_reduced70_asg.faa", "fasta"):
-#for record in SeqIO.parse("merged_bac_familyreps.faa", "fasta"):
-#for record in SeqIO.parse("class_rep_261_all_complete.faa", "fasta"):
-#for record in SeqIO.parse("bac_phylum_121_all_complete.faa", "fasta"):
-#for record in SeqIO.parse("all_complete_NCLDV_proteins.faa", "fasta"):
-#for record in SeqIO.parse("all_reduced343_ncldv.faa", "fasta"):
-#for record in SeqIO.parse("cured_merged_arc_familyreps.faa", "fasta"):
-#for record in SeqIO.parse("all_complete_reduced_euk.faa", "fasta"):
-#for record in SeqIO.parse("all_55_reduced_euk.faa", "fasta"):
-#for record in SeqIO.parse("all_87_reduced_euk.faa", "fasta"):
-#for record in SeqIO.parse("all_complete_Mirus_proteins.faa", "fasta"):
+for record in SeqIO.parse("all_proteins.faa", "fasta"):
+
 
   dict_id_seq[record.id] = record.seq
 #dict_id_seq
