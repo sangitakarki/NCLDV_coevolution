cf5c1d2aa0db06cbc17c9d4b7a515e5c9d3e2823
sangitakarki <77866879+sangitakarki@users.noreply.github.com>
2023-07-11 16:16:25 -0400

    Update 5.2depth_of_tree.py

--- 5.2depth_of_tree.py
+++ 5.2depth_of_tree.py
@@ -4,16 +4,12 @@ from collections import defaultdict
 from Bio.SeqRecord import SeqRecord
 from Bio.Seq import Seq
 from Bio import AlignIO
-from Bio.Align import MultipleSeqAlignment
 from Bio import Phylo
 
 list1 = []
 tree_file = "protein_umtree.nwk"
-#trees = Phylo.parse (tree_file, "newick")
 for tree in Phylo.parse(tree_file, "newick"):
     #tree = tree.root_at_midpoint()
-    #print(ss)
-    #print(tree.depth)
     tree_depth = tree.depths(unit_branch_lengths =False)
 
 for k, v in tree_depth.items():
